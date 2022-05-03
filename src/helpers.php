43087cf1418d33bd0191038faada0e45fd7bb50a
Yura Rodchyn <rodchyn+buddy@gmail.com>
2022-05-03 13:15:57 +0300

    Custom php-cs-fixer rules v0.0.1

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WorkplaceBuddy;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+function styles(Finder $finder, array $rules = []): Config {
+    
+    $rules = array_merge(require __DIR__.'/rules.php', $rules);
+
+    return (new Config())
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules($rules);
+}
---
+++ src/rules.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    '@PHP80Migration' => true,
+    '@PhpCsFixer' => true,
+    'strict_comparison' => false,
+    'strict_param' => false,
+    'psr_autoloading' => false,
+    'native_function_invocation' => false,
+    'declare_strict_types' => false,
+    'void_return' => false,
+];
